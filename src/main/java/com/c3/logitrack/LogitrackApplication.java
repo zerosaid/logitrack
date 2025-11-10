4e63f0d4c3de206002a58a78c8f35cceb65f120f
zerosaid <daniel235ub@gmail.com>
2025-11-10 14:58:18 -0500

    first commit

---
+++ src/main/java/com/c3/logitrack/LogitrackApplication.java
@@ -0,0 +1,13 @@
+package com.c3.logitrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogitrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogitrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/c3/logitrack/LogitrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c3.logitrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogitrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
