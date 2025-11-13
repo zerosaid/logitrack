7e7f4f10eda5c7c21a6ebf0d8666248dcd1826fc
JoshepMartinez <joshep543210@gmail.com>
2025-11-13 16:47:26 -0500

    Agregar descripciones a los roles en la enumeración Role para mejorar la claridad y comprensión del código

--- src/main/java/com/c3/logitrack/model/enums/Role.java
+++ src/main/java/com/c3/logitrack/model/enums/Role.java
@@ -1,6 +1,16 @@
 package com.c3.logitrack.model.enums;
 
 public enum Role {
-    ADMIN,
-    EMPLEADO
+    ADMIN("Administrador del sistema"),
+    EMPLEADO("Empleado operativo");
+
+    private final String descripcion;
+
+    Role(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
 }
