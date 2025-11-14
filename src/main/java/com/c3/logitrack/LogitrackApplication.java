a53baf4430444613bf8c62a8db88b3bab9e14f06
JoshepMartinez <joshep543210@gmail.com>
2025-11-14 18:01:49 -0500

    se le añadio una parte de prueba para ver si las credenciales las toman

--- src/main/java/com/c3/logitrack/LogitrackApplication.java
+++ src/main/java/com/c3/logitrack/LogitrackApplication.java
@@ -1,7 +1,10 @@
 package com.c3.logitrack;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class LogitrackApplication {
@@ -10,4 +13,15 @@ public class LogitrackApplication {
 		SpringApplication.run(LogitrackApplication.class, args);
 	}
 
+	@Bean
+    CommandLineRunner testHash(PasswordEncoder passwordEncoder) {
+        return args -> {
+            String hash = passwordEncoder.encode("adminPass01");
+            System.out.println("HASH CORRECTO PARA 'adminPass01': " + hash);
+
+            boolean match = passwordEncoder.matches("adminPass01",
+                "$2a$10$SBAgroaxJBDh3EiXYymSwO8iIOZ8XE1ZuYLyTGuZhHy1IGqkYc0uu");
+            System.out.println("¿EL HASH DE LA BD COINCIDE? " + match);
+        };
+    }
 }
