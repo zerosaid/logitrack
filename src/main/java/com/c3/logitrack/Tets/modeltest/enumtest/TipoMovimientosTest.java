840625265cbca2f49b2208fccf110b14a82eeac6
JoshepMartinez <joshep543210@gmail.com>
2025-11-21 16:32:55 -0500

    se creo los enum tanto para los tipos de movimientos como el insert, update, delete y tambien la operaciones que son entradas, salida y transferencia

---
+++ src/main/java/com/c3/logitrack/Tets/modeltest/enumtest/TipoMovimientosTest.java
@@ -0,0 +1,7 @@
+package com.c3.logitrack.Tets.modeltest.enumtest;
+
+public enum TipoMovimientosTest {
+    ENTRADA,
+    SALIDA,
+    TRANSFERENCIA;
+}
---
+++ src/main/java/com/c3/logitrack/Tets/modeltest/enumtest/TipoOperacionTest.java
@@ -0,0 +1,7 @@
+package com.c3.logitrack.Tets.modeltest.enumtest;
+
+public enum TipoOperacionTest {
+    INSERT,
+    UPDATE,
+    DELETE;
+}
